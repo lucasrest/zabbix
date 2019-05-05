5185962fcbc25374942de7323c461f52764d7baa
Lucas <lucas.edeploy@stelo.com.br>
2019-05-05 11:13:33 -0300

    Melhoria na  Configuração

--- src/main/java/config/ZabbixAlertaConfig.java
+++ src/main/java/config/ZabbixAlertaConfig.java
@@ -3,13 +3,32 @@ package config;
 import dominio.ZabbixAlerta;
 import servico.ZabbixAlertaServico;
 
+import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class ZabbixAlertaConfig {
 
-    public ZabbixAlertaConfig(ZabbixAlerta alerta) throws Exception {
-        ZabbixAlertaServico alertaServico = ZabbixAlertaServico.getInstance();
-        do {
-            alertaServico.enviar(alerta);
-            Thread.sleep(alerta.getIntervalo());
-        } while (true);
+    private static final String ZABBIX_ALERTA = "Zabbix Alerta";
+
+    private ZabbixAlertaServico alertaServico;
+
+    public ZabbixAlertaConfig(ZabbixAlerta alerta) {
+        alertaServico = ZabbixAlertaServico.getInstance();
+        enviarAlerta(alerta);
+    }
+
+    private void enviarAlerta(ZabbixAlerta alerta) {
+        TimerTask repeatedTask = new TimerTask() {
+            public void run() {
+                try {
+                    alertaServico.enviar(alerta);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        Timer alertaTimer = new Timer(ZABBIX_ALERTA);
+        alertaTimer.scheduleAtFixedRate(repeatedTask, 0, alerta.getIntervalo());
     }
 }
